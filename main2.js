c028023b144220db0734bd3296e7b75d21d3971c
Yutaka HARA <yutaka.hara.gmail.com>
2012-10-30 20:42:12 +0900

    support CallExpression

--- main2.js
+++ main2.js
@@ -6,21 +6,34 @@ var util = require("util"),
 
 Parser = require("./parser");
 parser = Parser.parser;
-Sym = Parser.Sym;
+isSymbol = function(x){
+  return x instanceof Parser.Symbol;
+}
 
 exprs = parser.parse(' \
-7 \
+(require "util") \
 ');
 
 util.puts(util.inspect(exprs, false, null, true));
 util.puts("--");
+
+function raise(msg, info){
+  info || (info = {});
+  info["ERROR"] = msg;
+  throw util.inspect(info);
+}
+
+function raiseIf(cond, msg, info){
+  if (cond) { raise(msg, info) }
+}
 
 function ast(typename, info){
   info["type"] = typename;
   return info;
 }
 
-function convert_value(v){
+// Returns JS-AST for Escodegen.
+function convertValue(v){
   if (_.isNumber(v) || _.isString(v) || _.isRegExp(v) ||
       _.isBoolean(v) || _.isNull(v) || _.isUndefined(v)){
     return ast("Literal", {value: v});
@@ -28,27 +41,33 @@ function convert_value(v){
   else if(_.isEmpty(v)){
     return ast("ObjectExpression", {properties: []});
   }
-  else if(v instanceof Parser.Symbol) {
+  else if(isSymbol(v)) {
     return ast("Identifier", {name: v.jsName});
+  }
+  else if(_.isArray(v)){ // application
+    var first = v[0].name, rest = v.slice(1);
+    switch(first){
+      case "define":
+        var left = e[1], rest = e.slice(2);
+        if (isSymbol(left))
+          return convertDefvar(left, rest);
+        else if (_.isArray(left))
+          return convertDefun(left, rest);
+        else
+          raise("malformed define", {left:left, rest:rest});
+      default:
+        return ast("CallExpression", {
+            callee: convertValue(v[0]),
+            arguments: rest.map(convertValue)
+          });
+        throw 3;
+    }
   }
   else {
     throw 2;
   }
 }
 
-function convert_toplevel(e){
-  if (_.isArray(e)){
-    throw 1;
-  }
-  else {
-    return convert_value(e);
-  }
-};
-
-function ast2js(ast){
-  return escodegen.generate(ast);
-};
-
 exprs.forEach(function(expr){
-  util.puts(ast2js(convert_toplevel(expr)));
+  util.puts(escodegen.generate(convertValue(expr)));
 });
